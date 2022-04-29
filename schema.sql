23d5fd3ff7a9820fb7455fd66a8eeae9caa65ecb
Ntare Guy <gntare2@gmail.com>
2022-04-29 15:08:18 +0200

    query tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,16 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE TABLE species (
+    id                  SERIAL PRIMARY KEY,
+    name                VARCHAR(250)
+);
+
+CREATE TABLE owners (
+    id                  SERIAL PRIMARY KEY,
+    full_name           VARCHAR(250),
+    age                 INT
+);
+
 CREATE TABLE animals (
     id                  INT GENERATED ALWAYS AS IDENTITY,
     name                VARCHAR(250),
@@ -8,6 +19,12 @@ CREATE TABLE animals (
     neutered            BOOLEAN,
     weight_kg           DECIMAL,
     PRIMARY KEY(id)
+    CONSTRAINT fk_specie
+        FOREIGN KEY(species_id)
+            REFERENCES species(id),
+    CONSTRAINT fk_owner
+        FOREIGN KEY(owner_id)
+            REFERENCES owners(id)
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(250);
